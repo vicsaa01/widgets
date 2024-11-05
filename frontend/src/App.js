33b5ceebd98fd4213050380506337d76d9996527
vicsaa01 <victorsaakes@gmail.com>
2024-11-05 17:08:12 +0100

    #001 - basic setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <div class="border border-3 border-dark">
+      <p>helo wold</p>
+    </div>
+  );
+}
+
+export default App;
