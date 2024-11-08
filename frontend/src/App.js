80765cc7a357044cba1708eece1e080220b1bee2
vicsaa01 <victorsaakes@gmail.com>
2024-11-08 13:02:16 +0100

    #002 - basic structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,32 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.css'
+
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom"
+
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+import Home from './pages/Home'
+import Calculator from './pages/Calculator'
+import Weather from './pages/Weather'
 
 function App() {
+
   return (
-    <div class="border border-3 border-dark">
-      <p>helo wold</p>
-    </div>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route path="/calculator" element={<Calculator/>}/>
+        <Route path="/weather" element={<Weather/>}/>
+      </Routes>
+      <Footer/>
+    </Router>
   );
 }
 
