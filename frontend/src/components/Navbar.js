584839126a67e77a4adb0ddafdf9a0431a678bbb
vicsaa01 <victorsaakes@gmail.com>
2024-11-14 17:56:41 +0100

    #007 - My name is Home, Home page

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     return(
         <nav>
             <div class="container-fluid">
-                <div class="row">
+                <div class="row w-100">
                     <div class="col pt-2 pb-2">
                         <a class="bg-white rounded me-3 p-0" href="/"><img class="rounded" src="logo.png" alt="Website Logo"></img></a>
                         <a class="btn rounded nav-button me-3 p-1" href="/calculator">Calculator</a>
