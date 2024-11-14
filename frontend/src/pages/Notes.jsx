2e027366b77c93a1dca82d287cf9fdb21e9318a3
vicsaa01 <victorsaakes@gmail.com>
2024-11-14 18:19:43 +0100

    #010 - new page style

--- frontend/src/pages/Notes.jsx
+++ frontend/src/pages/Notes.jsx
@@ -3,10 +3,16 @@ import React from 'react'
 const Notes = () => {
 
     return(
-        <main class="d-flex flex-col">
-            <p>Notes</p>
-            
-            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <main class="d-flex">
+            <div class="col-lg-2 col-md-1"></div>
+
+            <div class="col-lg-8 col-md-10 large-page">
+                <div class="row w-100 mt-5 mb-5">
+                    <h3 class="text-dark text-center">Notes</h3>
+                </div>
+            </div>
+
+            <div class="col-lg-2 col-md-1"></div>
         </main>
     )
 }
--- frontend/src/pages/Time.jsx
+++ frontend/src/pages/Time.jsx
@@ -3,10 +3,16 @@ import React from 'react'
 const Time = () => {
 
     return(
-        <main class="d-flex flex-col">
-            <p>Time</p>
-            
-            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <main class="d-flex">
+            <div class="col-lg-2 col-md-1"></div>
+
+            <div class="col-lg-8 col-md-10 large-page">
+                <div class="row w-100 mt-5 mb-5">
+                    <h3 class="text-dark text-center">Time</h3>
+                </div>
+            </div>
+
+            <div class="col-lg-2 col-md-1"></div>
         </main>
     )
 }
