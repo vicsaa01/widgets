19824cc3d5ace43c6c9c75452dfeee376ad7ac30
vicsaa01 <victorsaakes@gmail.com>
2024-11-14 15:20:13 +0100

    #006 - added /notes

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,10 +7,11 @@ const Navbar = () => {
             <div class="container-fluid">
                 <div class="row">
                     <div class="col pt-2 pb-2">
-                        <a class="bg-white rounded me-5 p-0" href="/"><img class="rounded" src="logo.png" alt="Website Logo"></img></a>
-                        <a class="btn rounded nav-button me-3" href="/calculator">Calculator</a>
-                        <a class="btn rounded nav-button me-3" href="/time">Time</a>
-                        <a class="btn rounded nav-button" href="/weather">Weather</a>
+                        <a class="bg-white rounded me-3 p-0" href="/"><img class="rounded" src="logo.png" alt="Website Logo"></img></a>
+                        <a class="btn rounded nav-button me-3 p-1" href="/calculator">Calculator</a>
+                        <a class="btn rounded nav-button me-3 p-1" href="/notes">Notes</a>
+                        <a class="btn rounded nav-button me-3 p-1" href="/time">Time</a>
+                        <a class="btn rounded nav-button p-1" href="/weather">Weather</a>
                     </div>
                 </div>
             </div>
---
+++ frontend/src/pages/Notes.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Notes = () => {
+
+    return(
+        <main class="d-flex flex-col">
+            <p>Notes</p>
+            
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        </main>
+    )
+}
+
+export default Notes
