cb0d28e134c29fc2722197c3648d818dfd9058e5
vicsaa01 <victorsaakes@gmail.com>
2024-11-14 18:07:45 +0100

    #009 - new icon

--- frontend/src/components/Widget.jsx
+++ frontend/src/components/Widget.jsx
@@ -4,7 +4,7 @@ const Widget = (props) => {
 
     return(
         <div class="col-lg-3 col-md-6 col-sm-6">
-            <a class="btn w-100 rounded border border-dark home-button mb-3" href={"/" + props.id}>
+            <a class="btn w-100 rounded border border-dark widget-button mb-3" href={"/" + props.id}>
                 <img class="p-3" src={"/img/" + props.id + ".png"} width="100" height="100"/>
                 <h5 class="mt-2">{props.name}</h5>
                 <p class="w-100">{props.description}</p>
